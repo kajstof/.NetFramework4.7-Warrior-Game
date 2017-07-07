a60d857bb49ebbddbd08dd547faf1b3f11b9f4aa
Krzysztof Krysiak <krysiak86@gmail.com>
2017-07-07 12:57:39 +0200

    Add project files.

---
+++ WarriorGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace WarriorGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Warrior krzys = new Warrior("Krzyś", 1000, 120, 40);
+            Warrior grzes = new Warrior("Grześ", 1000, 120, 40);
+            Battle.StartFight(krzys, grzes);
+            Console.ReadKey();
+        }
+    }
+}
